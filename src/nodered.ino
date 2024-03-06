1e27b5289f4f5173d80c896992f8674e2f26705e
nammonman <sirapat69@outlook.com>
2024-03-06 10:55:01 +0700

    sender condition

--- src/nodered.ino
+++ src/nodered.ino
@@ -19,23 +19,27 @@ struct Data {
 
 Data data;
 
-void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+void OnDataRecv(const uint8_t * mac_addr, const uint8_t *incomingData, int len) {
   memcpy(&data, incomingData, sizeof(data));
-  //sun angle
-  udp.beginPacket(udpAddress, 30000);
-  udp.write(reinterpret_cast<const uint8_t*>(std::to_string(data.y).c_str()), sizeof(const uint8_t)*std::to_string(data.y).length());
-  Serial.printf("angle: %d\n", data.y);
-  udp.endPacket();
-  //sun direction
-  udp.beginPacket(udpAddress, 30001);
-  udp.write(reinterpret_cast<const uint8_t*>(std::to_string(data.x).c_str()), sizeof(const uint8_t)*std::to_string(data.x).length());
-  Serial.printf("direction: %d\n", data.x);
-  udp.endPacket();
-  // avg brightness
-  udp.beginPacket(udpAddress, 30002);
-  udp.write(reinterpret_cast<const uint8_t*>(std::to_string(data.ldr).c_str()), sizeof(const uint8_t)*std::to_string(data.ldr).length());
-  Serial.printf("brightness: %d\n", data.ldr);
-  udp.endPacket();
+  uint8_t sender[] = { 0x3C, 0x61, 0x05, 0x03, 0x41, 0xA0 };
+  if (mac_addr[0] == sender[0] && mac_addr[1] == sender[1] && mac_addr[2] == sender[2] && mac_addr[3] == sender[3] && mac_addr[4] == sender[4] && mac_addr[5] == sender[5]) {
+    //sun angle
+    udp.beginPacket(udpAddress, 30000);
+    udp.write(reinterpret_cast<const uint8_t*>(std::to_string(data.y).c_str()), sizeof(const uint8_t)*std::to_string(data.y).length());
+    Serial.printf("angle: %d\n", data.y);
+    udp.endPacket();
+    //sun direction
+    udp.beginPacket(udpAddress, 30001);
+    udp.write(reinterpret_cast<const uint8_t*>(std::to_string(data.x).c_str()), sizeof(const uint8_t)*std::to_string(data.x).length());
+    Serial.printf("direction: %d\n", data.x);
+    udp.endPacket();
+    // avg brightness
+    udp.beginPacket(udpAddress, 30002);
+    udp.write(reinterpret_cast<const uint8_t*>(std::to_string(data.ldr).c_str()), sizeof(const uint8_t)*std::to_string(data.ldr).length());
+    Serial.printf("brightness: %d\n", data.ldr);
+    udp.endPacket();
+  }
+  
 }
 
 
